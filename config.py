39c53eb8b4d2964bd1dc2876cd174aa205d3c3b5
Tonglong Song <michael.song@unimelb.edu.au>
2023-05-30 16:16:32 +1000

    added config file added rotation of the local coordinate system to match projected WGS84 coordinate system

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import numpy as np
 DT_HOSTNAME = "115.146.94.74"
 DT_USERNAME = "ubuntu"
 DT_KEYFILEPATH = "app1server.pem"
-DT_IMGPATH = '/home/ubuntu/posecapture/tempdata/cam0/'
+DT_IMGPATH = '/home/ubuntu/posecapture/tempdata/DLAB/'
 
 DT_MQTT_HOST = 'mqtt.digitwin.com.au'
 DT_MQTT_USERNAME = 'csdila-3dpose-publisher'
@@ -20,10 +20,10 @@ REFERENCE_LOCAL = (0, 7)
 def get_cam_parameter():
     K = np.array([[611.1549814728781, 0.0, 959.5], [0.0, 611.1549814728781, 539.5], [0.0, 0.0, 1.0]])
     D = np.array([[0.0], [0.0], [0.0], [0.0]])
-    Rvec = np.array([[-0.5078205930618596, -0.8614604066248216, 0.002052578889661927],
-                     [-0.41821536497073863, 0.24444849680948733, -0.8748376083079499],
-                     [0.7531362119594922, -0.445118973113233, -0.48441195898731454]])
-    Tvec = np.array([[2548.5812958074703], [-974.8825087558083], [5950.456044850678]])
+    Rvec = np.array([[-0.5360341237889787, -0.8440653154164093, 0.014871497731556382],
+                     [-0.24532120093734894, 0.13888997427130634, -0.9594410265449218],
+                     [0.807765390756771, -0.517941423674448, -0.2815168825091088]])
+    Tvec = np.array([[2399.768884141257], [245.79184214661134], [6078.2605312897995]])
     DIM = (1920, 1080)
     return K, D, Rvec, Tvec, DIM
 
--- frame_receiver.py
+++ frame_receiver.py
@@ -45,6 +45,7 @@ def undistort(img_path):
     img = cv2.imread(img_path)
     map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
     undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    undistorted_img = cv2.flip(undistorted_img, -1)
     return undistorted_img
 
 
